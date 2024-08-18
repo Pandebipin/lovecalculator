4c0282a70b0eb0440c7fb660134ec948c8989637
bipin pandey <bp9777643@gmail.com>
2024-08-18 17:40:52 +0545

    making api secure

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3ehvtUvV_dTYLPEKBa68c2yMBtN5BPOY",
+  apiKey: import.meta.env.MY_VITE_KEY,
   authDomain: "lovepercentcal.firebaseapp.com",
   projectId: "lovepercentcal",
   storageBucket: "lovepercentcal.appspot.com",
